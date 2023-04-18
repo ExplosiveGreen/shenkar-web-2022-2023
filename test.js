0332d3af422685934ac8c39e984b9cb660a8a79f
Ron Barcochva <shoko6662@gmail.com>
2023-04-19 00:18:28 +0300

    finished experimenting with qr reader javascript cdn library work ok

---
+++ test.js
@@ -0,0 +1,10 @@
+const resultContainer = document.getElementById('qr-reader-results');
+let html5QrCode = new Html5Qrcode("qr-reader");
+function onScanSuccess(decodedText, decodedResult) {
+    resultContainer.innerHTML = `Scan result ${decodedText}`;
+    html5QrCode.stop();
+}
+// facingMode: "environment" mean use back camera
+// qrbox : width and height of scanning area
+// aspectRatio : of entire feed take automatically entire width but hight from the aspect ratio
+html5QrCode.start({ facingMode: "environment" }, { fps: 10, qrbox: {width: screen.width * 0.85, height: screen.width * 0.85}, aspectRatio: screen.width / screen.height }, onScanSuccess);
