e9051332d506e774c2b98ccd2f07259c68e2f8a8
Ron Barcochva <shoko6662@gmail.com>
2023-04-18 18:46:42 +0300

    remove redandent imformation

--- exercises/ex3/js/script.js
+++ exercises/ex3/js/script.js
@@ -1,24 +1,22 @@
-let letterList = [];
 let currentShow = null;
-let numletters = 0;
+let currentLettersList = null;
 let lastLetterVal;
 let newSection;
+let numletters = 0;
 let content = $("#content-area");
 $("#box-btn").on("click", function () {
   lastLetterVal = "A".charCodeAt(0) + Math.floor(numletters / 2);
-  letterList.push(
-    ...[
-      String.fromCharCode(lastLetterVal + (numletters++ % 2 == 0 ? 0 : 1)),
-      String.fromCharCode(lastLetterVal + (numletters++ % 2 == 0 ? 0 : 1)),
-      String.fromCharCode(lastLetterVal + (numletters++ % 2 == 0 ? 0 : 1)),
-    ].sort(() => 0.5 - Math.random())
-  );
+  currentLettersList = [
+    String.fromCharCode(lastLetterVal + (numletters++ % 2 == 0 ? 0 : 1)),
+    String.fromCharCode(lastLetterVal + (numletters++ % 2 == 0 ? 0 : 1)),
+    String.fromCharCode(lastLetterVal + (numletters++ % 2 == 0 ? 0 : 1)),
+  ].sort(() => 0.5 - Math.random())
   for (let i = 0; i < 3; i++) {
     newSection =  $('<section>').css({
       'width': ( content.children().last().width() + 20 || 80 ) + 'px',
       'height': ( content.children().last().height() + 20 || 80 ) + 'px',
     }).attr('id', 'box-' + content.children().length)
-    .html(`<p>${letterList[content.children().length]}</p>`);
+    .html(`<p>${currentLettersList[i]}</p>`);
     $(newSection.children()[0]).hide();
     newSection.on('click', function () {
       $(this.childNodes[0]).toggle();
