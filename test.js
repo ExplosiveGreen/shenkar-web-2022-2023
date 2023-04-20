0e8852eca0680d3405f9c6a7c54a85343a3cda6e
explosivegreen <shoko6662@gmail.com>
2023-04-20 23:49:10 +0300

    test scanner size

--- test.js
+++ test.js
@@ -8,5 +8,5 @@ function onScanSuccess(decodedText, decodedResult) {
 // facingMode: "environment" mean use back camera
 // qrbox : width and height of scanning area
 // aspectRatio : of entire feed take automatically entire width but hight from the aspect ratio
-html5QrCode.start({ facingMode: "environment" }, { fps: 10, qrbox: {width: screen.width * 0.85, height: screen.width * 0.85}, aspectRatio: screen.width / screen.height }, onScanSuccess);
+html5QrCode.start({ facingMode: "environment" }, { fps: 10, qrbox: {width: screen.width * 0.85, height: screen.width * 0.85} }, onScanSuccess);
 resultContainer.innerHTML = `${screen.width} x ${screen.height}`;
