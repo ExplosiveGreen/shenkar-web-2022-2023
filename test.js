bd68f8172f8629b19ccf6705533ac5561ee72492
explosivegreen <shoko6662@gmail.com>
2023-04-20 23:29:41 +0300

    test scanner size

--- test.js
+++ test.js
@@ -9,3 +9,4 @@ function onScanSuccess(decodedText, decodedResult) {
 // qrbox : width and height of scanning area
 // aspectRatio : of entire feed take automatically entire width but hight from the aspect ratio
 html5QrCode.start({ facingMode: "environment" }, { fps: 10, qrbox: {width: screen.width * 0.85, height: screen.width * 0.85}, aspectRatio: screen.width / screen.height }, onScanSuccess);
+resultContainer.innerHTML = `${screen.width} x ${screen.height}`;
